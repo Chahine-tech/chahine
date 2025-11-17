0781a9f596893ce664c6139e7de116b758345407
Chahine-tech <chahine.benlahcen@gmail.com>
2025-11-17 18:54:44 +0100

    fix: update experience data with correct start and end dates for roles

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -13,15 +13,23 @@ const PAST_ROLES: Role[] = [
     description:
       'Working on Generative AI within the R&D team, exploring and developing innovative solutions.',
     startDate: 'December 2023',
-    endDate: 'Now',
+    endDate: 'September 2025',
   },
   {
     company: 'Adcoin',
     role: 'Software Engineer (Apprenticeship)',
     description:
       'Developed and maintained software solutions in a fintech startup.',
-    startDate: 'April 2022',
+    startDate: 'September 2022',
     endDate: 'August 2023',
+  },
+  {
+    company: 'Adcoin',
+    role: 'Software Engineer (iternship)',
+    description:
+      'Developed and maintained software solutions in a fintech startup.',
+    startDate: 'April 2022',
+    endDate: 'August 2022',
   },
 ]
 
